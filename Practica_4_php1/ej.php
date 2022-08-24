0634207aab650f3a21732acac602da5f1d5aee0c
Agustín Córdoba <agustincordoba28@gmail.com>
2022-08-24 09:28:19 -0300

    practica 4 - ej 4

--- Practica_4_php1/ej.php
+++ Practica_4_php1/ej.php
@@ -1,16 +1,5 @@
-<html>
-<head><title>Documento 1</title></head>
-<body>
 <?php
- echo "<table width = 90% border = '1' >";
- $row = 5;
- $col = 2;
- for ($r = 1; $r <= $row; $r++) {
- echo "<tr>";
- for ($c = 1; $c <= $col;$c++) {
- echo "<td>&nbsp;</td>\n";
- } echo "</tr>\n";
- }
- echo "</table>\n";
+echo "El $flor $color \n";
+include 'datos.php';
+echo " El $flor $color";
 ?>
-</body></html>
