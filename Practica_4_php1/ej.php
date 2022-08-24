b41d938c5d754ec360826ff33a7cb127b694fc1e
Agustín Córdoba <agustincordoba28@gmail.com>
2022-08-24 08:40:23 -0300

    practica 4 - ej 2

--- Practica_4_php1/ej.php
+++ Practica_4_php1/ej.php
@@ -1,28 +1,14 @@
 <?php
-
-function doble($i) {
- return $i*2;
+$i = 1;
+switch ($i) {
+ case 0:
+ print "i equals 0";
+ break;
+ case 1:
+ print "i equals 1";
+ break;
+ case 2:
+ print "i equals 2";
+ break;
 }
-
-$a = TRUE;
-$b = "xyz";
-$c = 'xyz';
-$d = 12;
-echo gettype($a);
-echo gettype($b);
-echo gettype($c);
-echo gettype($d);
-
-if (is_int($d)) {
- $d += 4;
-}
-
-if (is_string($a)) {
- echo "Cadena: $a";
-}
-
-$d = $a ? ++$d : $d*3;
-$f = doble($d++);
-$g = $f += 10;
-echo $a, $b, $c, $d, $f , $g;
 ?>
